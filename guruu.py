aadf3721b785be3e0687df9cf331ca7386a3f2b8
sandhya <sandhyadsan03@gmail.com>
2026-06-18 18:44:21 +0530

    Remove unnecessary files

--- guruu.py
+++
@@ -1,9 +0,0 @@
-class Student:
-           def __init__ (self,name,age):
-                    self.name=name
-                    self.age=age
-           def display(self):
-                 print ("name:",self.name)
-                 print ("age:",self.age)
-s1=Student("Arun",22)
-s1.display()
